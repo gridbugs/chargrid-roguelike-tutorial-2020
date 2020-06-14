e71d5789e1f8f8948114b169c144e8a723185a1a
Stephen Sherratt <stephen@sherra.tt>
2020-06-14 18:44:52 +1000

    part-1.1: Drawing a @ sign

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use coord_2d::{Coord, Size};
+use rgb24::Rgb24;
+
 fn main() {
     use chargrid_graphical::{Context, ContextDescriptor, Dimensions, FontBytes};
     const CELL_SIZE_PX: f64 = 24.;
@@ -27,15 +30,56 @@ fn main() {
         underline_top_offset: 0.8,
     })
     .expect("Failed to initialize graphical context");
-    let app = App::new();
+    let screen_size = Size::new(40, 30);
+    let app = App::new(screen_size);
     context.run_app(app);
 }
 
-struct App {}
+struct AppData {
+    player_coord: Coord,
+}
 
-impl App {
+impl AppData {
+    fn new(screen_size: Size) -> Self {
+        Self {
+            player_coord: screen_size.to_coord().unwrap() / 2,
+        }
+    }
+}
+
+struct AppView {}
+
+impl AppView {
     fn new() -> Self {
         Self {}
+    }
+}
+
+impl<'a> chargrid::render::View<&'a AppData> for AppView {
+    fn view<F: chargrid::app::Frame, C: chargrid::app::ColModify>(
+        &mut self,
+        data: &'a AppData,
+        context: chargrid::app::ViewContext<C>,
+        frame: &mut F,
+    ) {
+        let view_cell = chargrid::render::ViewCell::new()
+            .with_character('@')
+            .with_foreground(Rgb24::new_grey(255));
+        frame.set_cell_relative(data.player_coord, 0, view_cell, context);
+    }
+}
+
+struct App {
+    data: AppData,
+    view: AppView,
+}
+
+impl App {
+    fn new(screen_size: Size) -> Self {
+        Self {
+            data: AppData::new(screen_size),
+            view: AppView::new(),
+        }
     }
 }
 
@@ -52,13 +96,15 @@ impl chargrid::app::App for App {
     fn on_frame<F, C>(
         &mut self,
         _since_last_frame: chargrid::app::Duration,
-        _view_context: chargrid::app::ViewContext<C>,
-        _frame: &mut F,
+        view_context: chargrid::app::ViewContext<C>,
+        frame: &mut F,
     ) -> Option<chargrid::app::ControlFlow>
     where
         F: chargrid::app::Frame,
         C: chargrid::app::ColModify,
     {
+        use chargrid::render::View;
+        self.view.view(&self.data, view_context, frame);
         None
     }
 }

2a4c7aca2c15a88ffa2d208f95b17e21d9d77dba
Stephen Sherratt <stephen@sherra.tt>
2020-06-14 18:30:43 +1000

    part-1.0: Empty window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,64 @@
 fn main() {
-    println!("Hello, world!");
+    use chargrid_graphical::{Context, ContextDescriptor, Dimensions, FontBytes};
+    const CELL_SIZE_PX: f64 = 24.;
+    let context = Context::new(ContextDescriptor {
+        font_bytes: FontBytes {
+            normal: include_bytes!("./fonts/PxPlus_IBM_CGAthin.ttf").to_vec(),
+            bold: include_bytes!("./fonts/PxPlus_IBM_CGA.ttf").to_vec(),
+        },
+        title: "Chargrid Tutorial".to_string(),
+        window_dimensions: Dimensions {
+            width: 960.,
+            height: 720.,
+        },
+        cell_dimensions: Dimensions {
+            width: CELL_SIZE_PX,
+            height: CELL_SIZE_PX,
+        },
+        font_dimensions: Dimensions {
+            width: CELL_SIZE_PX,
+            height: CELL_SIZE_PX,
+        },
+        font_source_dimensions: Dimensions {
+            width: CELL_SIZE_PX as f32,
+            height: CELL_SIZE_PX as f32,
+        },
+        underline_width: 0.1,
+        underline_top_offset: 0.8,
+    })
+    .expect("Failed to initialize graphical context");
+    let app = App::new();
+    context.run_app(app);
+}
+
+struct App {}
+
+impl App {
+    fn new() -> Self {
+        Self {}
+    }
+}
+
+impl chargrid::app::App for App {
+    fn on_input(&mut self, input: chargrid::app::Input) -> Option<chargrid::app::ControlFlow> {
+        use chargrid::input::{keys, Input};
+        match input {
+            Input::Keyboard(keys::ETX) | Input::Keyboard(keys::ESCAPE) => {
+                Some(chargrid::app::ControlFlow::Exit)
+            }
+            _ => None,
+        }
+    }
+    fn on_frame<F, C>(
+        &mut self,
+        _since_last_frame: chargrid::app::Duration,
+        _view_context: chargrid::app::ViewContext<C>,
+        _frame: &mut F,
+    ) -> Option<chargrid::app::ControlFlow>
+    where
+        F: chargrid::app::Frame,
+        C: chargrid::app::ColModify,
+    {
+        None
+    }
 }

0b623bf373cabf872eef6d391de92c53a2c9f5d4
Stephen Sherratt <stephen@sherra.tt>
2020-06-21 16:17:50 +1000

    part-2.3: Locations tracked with spatial table

--- src/game.rs
+++ src/game.rs
@@ -9,27 +9,47 @@ pub enum Tile {
 
 entity_table::declare_entity_module! {
     components {
-        coord: Coord,
         tile: Tile,
     }
 }
 
 use components::Components;
 
+spatial_table::declare_layers_module! {
+    layers {
+        floor: Floor,
+        character: Character,
+        feature: Feature,
+    }
+}
+
+pub use layers::Layer;
+type SpatialTable = spatial_table::SpatialTable<layers::Layers>;
+pub type Location = spatial_table::Location<Layer>;
+
 pub struct GameState {
     screen_size: Size,
     components: Components,
+    spatial_table: SpatialTable,
     player_entity: Entity,
 }
 
 pub struct EntityToRender {
     pub tile: Tile,
-    pub coord: Coord,
+    pub location: Location,
 }
 
 impl GameState {
     fn spawn_player(&mut self, coord: Coord) {
-        self.components.coord.insert(self.player_entity, coord);
+        self.spatial_table
+            .update(
+                self.player_entity,
+                Location {
+                    coord,
+                    layer: Some(Layer::Character),
+                },
+            )
+            .unwrap();
         self.components
             .tile
             .insert(self.player_entity, Tile::Player);
@@ -40,10 +60,12 @@ impl GameState {
     pub fn new(screen_size: Size) -> Self {
         let mut entity_allocator = EntityAllocator::default();
         let components = Components::default();
+        let spatial_table = SpatialTable::new(screen_size);
         let player_entity = entity_allocator.alloc();
         let mut game_state = Self {
             screen_size,
             components,
+            spatial_table,
             player_entity,
         };
         game_state.populate(screen_size.to_coord().unwrap() / 2);
@@ -51,21 +73,22 @@ impl GameState {
     }
     pub fn maybe_move_player(&mut self, direction: CardinalDirection) {
         let player_coord = self
-            .components
-            .coord
-            .get_mut(self.player_entity)
-            .expect("player has no coord component");
-        let new_player_coord = *player_coord + direction.coord();
+            .spatial_table
+            .coord_of(self.player_entity)
+            .expect("player has no coord");
+        let new_player_coord = player_coord + direction.coord();
         if new_player_coord.is_valid(self.screen_size) {
-            *player_coord = new_player_coord;
+            self.spatial_table
+                .update_coord(self.player_entity, new_player_coord)
+                .unwrap();
         }
     }
     pub fn entities_to_render<'a>(&'a self) -> impl 'a + Iterator<Item = EntityToRender> {
         let tile_component = &self.components.tile;
-        let coord_component = &self.components.coord;
+        let spatial_table = &self.spatial_table;
         tile_component.iter().filter_map(move |(entity, &tile)| {
-            let coord = coord_component.get(entity).cloned()?;
-            Some(EntityToRender { tile, coord })
+            let &location = spatial_table.location_of(entity)?;
+            Some(EntityToRender { tile, location })
         })
     }
 }

d982e08a0c24407101eccd440f0a94fb901f653a
Stephen Sherratt <stephen@sherra.tt>
2020-06-21 15:34:07 +1000

    part-2.1: Generic entities

--- src/game.rs
+++ src/game.rs
@@ -1,25 +1,65 @@
 use coord_2d::{Coord, Size};
 use direction::CardinalDirection;
+use entity_table::{Entity, EntityAllocator};
+
+#[derive(Clone, Copy, Debug)]
+pub enum Tile {
+    Player,
+}
+
+entity_table::declare_entity_module! {
+    components {
+        coord: Coord,
+        tile: Tile,
+    }
+}
+
+use components::Components;
 
 pub struct GameState {
     screen_size: Size,
-    player_coord: Coord,
+    components: Components,
+    player_entity: Entity,
 }
 
 impl GameState {
+    fn spawn_player(&mut self, coord: Coord) {
+        self.components.coord.insert(self.player_entity, coord);
+        self.components
+            .tile
+            .insert(self.player_entity, Tile::Player);
+    }
+    fn populate(&mut self, player_coord: Coord) {
+        self.spawn_player(player_coord);
+    }
     pub fn new(screen_size: Size) -> Self {
-        Self {
+        let mut entity_allocator = EntityAllocator::default();
+        let components = Components::default();
+        let player_entity = entity_allocator.alloc();
+        let mut game_state = Self {
             screen_size,
-            player_coord: screen_size.to_coord().unwrap() / 2,
-        }
+            components,
+            player_entity,
+        };
+        game_state.populate(screen_size.to_coord().unwrap() / 2);
+        game_state
     }
     pub fn maybe_move_player(&mut self, direction: CardinalDirection) {
-        let new_player_coord = self.player_coord + direction.coord();
+        let player_coord = self
+            .components
+            .coord
+            .get_mut(self.player_entity)
+            .expect("player has no coord component");
+        let new_player_coord = *player_coord + direction.coord();
         if new_player_coord.is_valid(self.screen_size) {
-            self.player_coord = new_player_coord;
+            *player_coord = new_player_coord;
         }
     }
     pub fn player_coord(&self) -> Coord {
-        self.player_coord
+        *self
+            .components
+            .coord
+            .get(self.player_entity)
+            .expect("player has no coord component")
     }
 }

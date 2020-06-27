4df426b34765b1a64aab611b2e713376a7f44051
Stephen Sherratt <stephen@sherra.tt>
2020-06-27 22:34:36 +1000

    part-4.1: Add field-of-view

--- src/game.rs
+++ src/game.rs
@@ -1,3 +1,4 @@
+use crate::visibility::{CellVisibility, VisibilityGrid};
 use crate::world::{Location, Populate, Tile, World};
 use coord_2d::Size;
 use direction::CardinalDirection;
@@ -8,11 +9,14 @@ use rand_isaac::Isaac64Rng;
 pub struct EntityToRender {
     pub tile: Tile,
     pub location: Location,
+    pub visibility: CellVisibility,
 }
 
 pub struct GameState {
     world: World,
     player_entity: Entity,
+    shadowcast_context: shadowcast::Context<u8>,
+    visibility_grid: VisibilityGrid,
 }
 
 impl GameState {
@@ -20,10 +24,16 @@ impl GameState {
         let mut world = World::new(screen_size);
         let mut rng = Isaac64Rng::from_entropy();
         let Populate { player_entity } = world.populate(&mut rng);
-        Self {
+        let shadowcast_context = shadowcast::Context::default();
+        let visibility_grid = VisibilityGrid::new(screen_size);
+        let mut game_state = Self {
             world,
             player_entity,
-        }
+            shadowcast_context,
+            visibility_grid,
+        };
+        game_state.update_visibility();
+        game_state
     }
     pub fn maybe_move_player(&mut self, direction: CardinalDirection) {
         self.world
@@ -32,9 +42,24 @@ impl GameState {
     pub fn entities_to_render<'a>(&'a self) -> impl 'a + Iterator<Item = EntityToRender> {
         let tile_component = &self.world.components.tile;
         let spatial_table = &self.world.spatial_table;
+        let visibility_grid = &self.visibility_grid;
         tile_component.iter().filter_map(move |(entity, &tile)| {
             let &location = spatial_table.location_of(entity)?;
-            Some(EntityToRender { tile, location })
+            let visibility = visibility_grid.cell_visibility(location.coord);
+            Some(EntityToRender {
+                tile,
+                location,
+                visibility,
+            })
         })
+    }
+    pub fn update_visibility(&mut self) {
+        let player_coord = self
+            .world
+            .spatial_table
+            .coord_of(self.player_entity)
+            .unwrap();
+        self.visibility_grid
+            .update(player_coord, &self.world, &mut self.shadowcast_context);
     }
 }

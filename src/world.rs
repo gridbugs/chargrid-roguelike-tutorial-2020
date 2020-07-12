64ecceda0c4830a206d8e10a0830a31e90a457e0
Stephen Sherratt <stephen@sherra.tt>
2020-07-12 11:51:32 +1000

    part-6.4: Combat system

--- src/world.rs
+++ src/world.rs
@@ -5,6 +5,18 @@ use direction::CardinalDirection;
 use entity_table::{ComponentTable, Entity, EntityAllocator};
 use rand::Rng;
 
+#[derive(Clone, Copy, Debug)]
+pub struct HitPoints {
+    pub current: u32,
+    pub max: u32,
+}
+
+impl HitPoints {
+    fn new_full(max: u32) -> Self {
+        Self { current: max, max }
+    }
+}
+
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum NpcType {
     Orc,
@@ -23,15 +35,18 @@ impl NpcType {
 #[derive(Clone, Copy, Debug)]
 pub enum Tile {
     Player,
+    PlayerCorpse,
     Floor,
     Wall,
     Npc(NpcType),
+    NpcCorpse(NpcType),
 }
 
 entity_table::declare_entity_module! {
     components {
         tile: Tile,
         npc_type: NpcType,
+        hit_points: HitPoints,
     }
 }
 
@@ -41,6 +56,7 @@ spatial_table::declare_layers_module! {
     layers {
         floor: Floor,
         character: Character,
+        corpse: Corpse,
         feature: Feature,
     }
 }
@@ -109,6 +125,9 @@ impl World {
             )
             .unwrap();
         self.components.tile.insert(entity, Tile::Player);
+        self.components
+            .hit_points
+            .insert(entity, HitPoints::new_full(20));
         entity
     }
     fn spawn_npc(&mut self, coord: Coord, npc_type: NpcType) -> Entity {
@@ -124,6 +143,11 @@ impl World {
             .unwrap();
         self.components.tile.insert(entity, Tile::Npc(npc_type));
         self.components.npc_type.insert(entity, npc_type);
+        let hit_points = match npc_type {
+            NpcType::Orc => HitPoints::new_full(2),
+            NpcType::Troll => HitPoints::new_full(6),
+        };
+        self.components.hit_points.insert(entity, hit_points);
         entity
     }
     pub fn populate<R: Rng>(&mut self, rng: &mut R) -> Populate {
@@ -154,23 +178,65 @@ impl World {
         }
     }
     pub fn maybe_move_character(&mut self, character_entity: Entity, direction: CardinalDirection) {
-        let player_coord = self
+        let character_coord = self
             .spatial_table
             .coord_of(character_entity)
-            .expect("player has no coord");
-        let new_player_coord = player_coord + direction.coord();
-        if new_player_coord.is_valid(self.spatial_table.grid_size()) {
-            let dest_layers = self.spatial_table.layers_at_checked(new_player_coord);
-            if let Some(character) = dest_layers.character {
-                if let Some(npc_type) = self.components.npc_type.get(character) {
-                    println!("You harmlessly bump into the {}.", npc_type.name());
+            .expect("character has no coord");
+        let new_character_coord = character_coord + direction.coord();
+        if new_character_coord.is_valid(self.spatial_table.grid_size()) {
+            let dest_layers = self.spatial_table.layers_at_checked(new_character_coord);
+            if let Some(dest_character_entity) = dest_layers.character {
+                let character_is_npc = self.components.npc_type.contains(character_entity);
+                let dest_character_is_npc =
+                    self.components.npc_type.contains(dest_character_entity);
+                if character_is_npc != dest_character_is_npc {
+                    self.character_bump_attack(dest_character_entity);
                 }
             } else if dest_layers.feature.is_none() {
                 self.spatial_table
-                    .update_coord(character_entity, new_player_coord)
+                    .update_coord(character_entity, new_character_coord)
                     .unwrap();
             }
         }
+    }
+    fn character_bump_attack(&mut self, victim: Entity) {
+        const DAMAGE: u32 = 1;
+        if let Some(hit_points) = self.components.hit_points.get_mut(victim) {
+            hit_points.current = hit_points.current.saturating_sub(DAMAGE);
+            if hit_points.current == 0 {
+                self.character_die(victim);
+            }
+        }
+    }
+    fn character_die(&mut self, entity: Entity) {
+        if let Some(occpied_by_entity) = self
+            .spatial_table
+            .update_layer(entity, Layer::Corpse)
+            .err()
+            .map(|e| e.unwrap_occupied_by())
+        {
+            // If a character dies on a cell which contains a corpse, remove the existing corpse
+            // from existence and replace it with the character's corpse.
+            self.remove_entity(occpied_by_entity);
+            self.spatial_table
+                .update_layer(entity, Layer::Corpse)
+                .unwrap();
+        }
+        let current_tile = self.components.tile.get(entity).unwrap();
+        let corpse_tile = match current_tile {
+            Tile::Player => Tile::PlayerCorpse,
+            Tile::Npc(npc_type) => Tile::NpcCorpse(*npc_type),
+            other => panic!("unexpected tile on character {:?}", other),
+        };
+        self.components.tile.insert(entity, corpse_tile);
+    }
+    pub fn is_living_character(&self, entity: Entity) -> bool {
+        self.spatial_table.layer_of(entity) == Some(Layer::Character)
+    }
+    pub fn remove_entity(&mut self, entity: Entity) {
+        self.components.remove_entity(entity);
+        self.spatial_table.remove(entity);
+        self.entity_allocator.free(entity);
     }
     pub fn size(&self) -> Size {
         self.spatial_table.grid_size()
